c963e97ee87cbd09643220568c690de027b02ffc
Nozer-Taz <111nazarbektajibaev@gmail.com>
2024-10-20 00:34:27 +0600

    Homework 2

--- hero.py
+++ hero.py
@@ -21,11 +21,79 @@ class SuperHero:
 
     def __sizeof__(self):
         return f'Catchphrase length: {len(self.catchphrase)}'
+
+
+class CosmoHero(SuperHero):
+
+    def __init__(self, name, nickname, superpower, health_points, catchphrase, damage):
+        super().__init__(name, nickname, superpower, health_points, catchphrase)
+        self.damage = damage
+        self.fly = False
+
+    @property
+    def who_am_i(self):
+        return f'I am {self.nickname}, my catchphrase is - {self.catchphrase}'
     
+    def health_multiply(self):
+        self.health_points **= 2
+        self.fly = True
+        return self.health_points, self.fly
+    
+    def true_phrase(self):
+        return 'True in the True_phrase'
 
-hero1 = SuperHero(name='Bruce', nickname='Batman', superpower='will power', health_points=100, catchphrase='You either die a hero or live long enough to see yourself become the villain')
 
-print(hero1.say_my_name())
-print(hero1.health_multiply())
-print(hero1.__str__())
-print(hero1.__sizeof__())
+class AeroHero(SuperHero):
+    def __init__(self, name, nickname, superpower, health_points, catchphrase, damage):
+        super().__init__(name, nickname, superpower, health_points, catchphrase)
+        self.damage = damage
+        self.fly = False
+    
+    @property
+    def who_am_i(self):
+        return f'I am {self.nickname}, my catchphrase is - {self.catchphrase}'
+
+    def health_multiply(self):
+        self.health_points **= 2
+        self.fly = True
+        return self.health_points, self.fly
+
+
+    def true_phrase(self):
+        return 'True in the True_phrase'
+
+
+cos_hero = CosmoHero('Cosmos', 'Univa', 'Black hole creation', 50, 'Ahah Lol, catch a black hole', 10)
+
+aer_hero = AeroHero('Aang', 'Avatar', 'Avatar experience', 60, 'Please don\'t bully me', 20)
+
+print(cos_hero.true_phrase())
+print(cos_hero.health_points)
+print(cos_hero.health_multiply())
+print(cos_hero.health_points)
+print(cos_hero.who_am_i)
+
+print(aer_hero.true_phrase())
+print(aer_hero.health_points)
+print(aer_hero.health_multiply())
+print(aer_hero.health_points)
+print(aer_hero.who_am_i)
+
+
+class Villain(CosmoHero):
+    people = 'monster'
+
+    def __init__(self, name, nickname, superpower, health_points, catchphrase, damage):
+        super().__init__(name, nickname, superpower, health_points, catchphrase, damage)
+    
+    def gen_x(self):
+        pass
+
+    def crit(self):
+        self.damage **= 1.5
+        return self.damage
+
+vill1 = Villain('Zuko', 'Blue demon', 'Fire bender', 100, 'I will restore my honor', 55)
+vill2 = Villain('Azula', 'Crazy', 'Fire bending', 120, 'My mother called me a monster', 60)
+
+print(vill2.crit())
